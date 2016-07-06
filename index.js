ef916970003230ff595076851e70192ad273e191
Marlon Chavarria <mchavarriae@gmail.com>
2016-07-05 22:28:17 -0600

    Back to original state

--- index.js
+++ index.js
@@ -12,32 +12,31 @@ var consoleLog = console.log.bind(console);
 
 function httpRequestAppender(config) {
     return function (loggingEvent) {
-        consoleLog(JSON.stringify(loggingEvent));
+
         //validate configuration
         if (config != null && config.httpRequest != null && config.httpRequest.endpoint != null &&
             config.httpRequest.method != null && config.httpRequest.contentType != null &&
             config.params != null && config.paramUsedToSendLoggingEvent) {
-            var options = {
-                method: config.httpRequest.method,
+            var options = { method: config.httpRequest.method,
                 url: config.httpRequest.endpoint,
-                headers: {'content-type': config.httpRequest.contentType}
+                headers: { 'content-type': config.httpRequest.contentType}
             };
 
             if (config.httpRequest.contentType === 'application/x-www-form-urlencoded') {
-                options.form = config.params;
-                options.form.metadata = JSON.stringify(loggingEvent);
+                options.form = config.params
+                options.form[config.paramUsedToSendLoggingEvent] = loggingEvent;
 
             } else if (config.httpRequest.contentType === 'application/json') {
                 options.body = config.params;
-                options.body.metadata = loggingEvent;
+                options.body[config.paramUsedToSendLoggingEvent] = loggingEvent;
             } else {
                 consoleLog("ContentType: ".concat(config.httpRequest.contentType, " is not supported"));
             }
-        } else {
+        }else{
             consoleLog("httpRequestAppender: Invalid Configuration please check");
             return;
         }
-        consoleLog(JSON.stringify(options));
+
 
         request(options, function (error, response, body) {
             if (error) console.log("Error from httpRequestAppender: ".concat(JSON.stringify(error)));
