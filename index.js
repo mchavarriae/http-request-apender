3571e742d7f36e7be25cf515fe1a2fb959ddcaa8
Marlon Chavarria <mchavarriae@gmail.com>
2016-07-05 21:59:56 -0600

    Pruebas

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function httpRequestAppender(config) {
 
             if (config.httpRequest.contentType === 'application/x-www-form-urlencoded') {
                 options.form = config.params;
-                options.form.metadata = loggingEvent;
+                options.form.metadata = JSON.stringify(loggingEvent);
 
             } else if (config.httpRequest.contentType === 'application/json') {
                 options.body = config.params;
